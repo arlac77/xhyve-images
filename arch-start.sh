5c8769ab88b860038eb6be1f73541439b22f2c0b
Markus Felten <Markus.Felten@gmx.de>
2019-10-04 22:42:25 +0200

    net still not working with mac addr

--- arch-start.sh
+++ arch-start.sh
@@ -10,15 +10,24 @@ DISK=${MACHINE}/linux.img
 UUID=61CF84A9-E75E-4237-9D21-285EE98D114D
 CPUS=1
 MEMORY=1G
+MAC=42:22:b1:3b:bd:90
+NET=virtio-net
+#NET=virtio-tap
 
 . ${MACHINE}/settings.ini
+
+NET_PCI="-s 2,${NET},tap0,mac=${MAC}"
+
+echo ${NET_PCI}
+
+#exit
 
 sudo xhyve \
     -A \
     -c ${CPUS} \
     -m ${MEMORY} \
     -s 0,hostbridge \
-    -s 2,virtio-net \
+    ${NET_PCI} \
     -s 4,virtio-blk,${DISK} \
     -s 31,lpc \
     -l com1,stdio \
